4e74cb7817d45c1144c5d3bb526e93276aecb75e
lazlanrafar <lazlanrafar@gmail.com>
2022-05-18 09:15:59 +0700

    add model auth

---
+++ models/entity.auth.go
@@ -0,0 +1,12 @@
+package model
+
+import "time"
+
+type User struct {
+	ID        uint   `gorm:"primary_key" json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	UpdatedAt time.Time
+	CreatedAt time.Time
+}
