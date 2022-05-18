1405cbd92bb1a2902afe40f72f144173bc928ce4
lazlanrafar <lazlanrafar@gmail.com>
2022-05-18 08:58:49 +0700

    done results project

--- routes/route.project.go
+++ routes/route.project.go
@@ -3,13 +3,16 @@ package route
 import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+
+	resultsProject "lazlanrafar/controllers/project-controllers/results"
 )
 
 func InitProjectRoutes(db *gorm.DB, router *gin.RouterGroup) {
+	// Project results
+	resultsProjectRepository := resultsProject.NewRepositoryResults(db)
+	resultsProjectService := resultsProject.NewServiceResults(resultsProjectRepository)
+	resultsProjectHandler := resultsProject.NewHandlerResults(resultsProjectService)
 
-	router.GET("/project", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	router.GET("/project", resultsProjectHandler.ResultsProjectHandler)
+
 }
